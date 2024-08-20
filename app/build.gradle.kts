e6b65c8e482711b18e95a87c5bd2367a908b6087
Dhruv Bansal <dhruvtgs003@gmail.com>
2024-08-21 01:33:17 +0530

    code visualization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dhruv.angularapps"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
